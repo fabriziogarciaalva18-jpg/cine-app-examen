adee85a1de759e54b9e799116b27e0c3d27b4bd1
Fabrizio Garcia <fabriziogarciaalva18@gmail.com>
2026-06-03 20:39:28 -0500

    Agregar Tailwind CSS correctamente

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
 
     @routes
     <!-- ¡Crucial! Verificamos que cargue tanto el CSS como el JS en la ruta exacta -->
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body class="font-sans antialiased bg-slate-900 text-slate-100">
     @inertia
