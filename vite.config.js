90109a8da11ca3a09826b2b51cc462942e051962
Fabrizio Garcia <fabriziogarciaalva18@gmail.com>
2026-06-03 20:48:45 -0500

    Corregir app.css con sintaxis clasica de Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -13,10 +12,9 @@ export default defineConfig({
             template: {
                 transformAssetUrls: {
                     base: null,
-                    includeAbsolute: false,
+                    includeAbsolute: true,
                 },
             },
         }),
-        tailwindcss(),
     ],
 });
